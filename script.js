ed9c5ffa3cc773913dd9dafee9507e0479046bfe
Maxuel Lima <maxuellima20@gmail.com>
2023-05-31 23:58:02 -0300

    Exercícios de laços feitos em aula

--- script.js
+++ script.js
@@ -1,8 +1,45 @@
-// --------------- PRÁTICA GUIADA ---------------
+// // --------------- PRÁTICA GUIADA ---------------
+// const bimestre1 = [9, 8, 7,6]
+// const bimestre2 = [5, 6,7, 8]
+// const bimestre3 = [4, 6, 8, 10]
+// const bimestre4 = [3, 5, 7, 9]
 
+// const notasDoAno = [bimestre1, bimestre2, bimestre3, bimestre4]
 
+// // console.table(notasDoAno)
 
+// // for(let i = 0; i < notasDoAno.length;i++){
+// //   let soma = 0
+// //   for(let j = 0; j <notasDoAno[i].length; j++){
+// //     soma = soma + notasDoAno[i][j]
 
+// //   }
+// //   let mediaBimestre = soma / notasDoAno[i].length
+
+// //   console.log(`A soma da média do bimestre ${i+1} foi ${soma} \ne a média do bimestre ${i+1} foi ${mediaBimestre}`)
+// // }
+
+// // const bimestres = [[9, 8, 7, 6], [5, 6, 7, 8], [4, 6, 8, 10], [3, 5, 7, 9]]
+
+// // for (let i = 0; i < bimestres.length; i++) {
+// //   let soma = 0;
+// //   for (let j = 0; j < bimestres[i].length; j++) {
+// //     soma = bimestres[i][j] + soma
+// //   }
+// //   console.log(`Soma do bimestre ${i + 1} foi ${soma}`)
+// // }
+// for(let indiceBimestre in notasDoAno){
+//   console.log(`Índice no array é ${indiceBimestre}`)
+//   let soma = 0
+
+//   for(let nota of notasDoAno[indiceBimestre]){
+//     soma = soma + nota
+//   }
+
+//   let mediaBimestre = soma / notasDoAno[indiceBimestre].length
+
+//   console.log(`A soma da média do bimestre ${Number(indiceBimestre)+1} foi ${soma} \ne a média do bimestre ${Number(indiceBimestre)+1} foi ${mediaBimestre}`)
+// }
 
 
 
@@ -56,4 +93,25 @@ const filmes = [
   },
 ]
 
+
+// for (let i = 0; i < filmes.length; i++) {
+//   console.log(`\n\nTítulo do filme: ${filmes[i].titulo} \nAno: ${filmes[i].ano}\nDireção: ${filmes[i].diretor}\n`);
+//   for (let j = 0; j < filmes[i].elenco.length; j++) {
+
+//     console.log(`\nAtor/atriz: ${filmes[i].elenco[j]}`);
+
+//   }
+
+// }
+
 // escreva seu código abaixo 👇🏻
+
+for (let i in filmes) {
+  console.log(`\n\nTítulo do filme: ${filmes[i].titulo} \nAno: ${filmes[i].ano}\nDireção: ${filmes[i].diretor}\n`);
+  for (let atriz of filmes[i].elenco) {
+
+    console.log(`\nAtor/atriz: ${atriz}`);
+
+  }
+
+}
